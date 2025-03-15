bed9539e0a8ca000cd95e7c2d55c857435e24957
Wuid0801 <pig10326@naver.com>
2025-03-15 11:48:01 +0900

    Fix : wallet

--- src/components/WalletLayout.tsx
+++ src/components/WalletLayout.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Wallet from "./wallet/Wallet";
 
 const WalletLayout: React.FC = () => {
-  const refreshToken = localStorage.getItem("privy:refresh_token");
+  const [refreshToken, setRefreshToken] = useState<string | null>(null);
+
+  if (!refreshToken) {
+    setRefreshToken(localStorage.getItem("privy:refresh_token"));
+  }
 
   return (
     <>
