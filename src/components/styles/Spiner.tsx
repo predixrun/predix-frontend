dc7e056c38389e78d52dd809096b5f3722f03d0f
Wuid0801 <pig10326@naver.com>
2025-03-25 23:52:43 +0900

    Fix : transfer

--- src/components/styles/Spiner.tsx
+++ src/components/styles/Spiner.tsx
@@ -2,7 +2,6 @@ import "@/components/styles/game-dashboard-animations.css";
 const Spinner = () => (
   <div className="loading-wrap">
     <div className="loading-spinner"></div>
-    <p className="text-lg text-[#B3B3B3] mt-4">Loading...</p>
   </div>
 );
 
